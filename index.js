0bc057065971c3f7ea88aee90c8bd19fadc0ba6b
RafaelIsr <146373789+RafaelIsr@users.noreply.github.com>
2023-11-27 21:38:56 +0500

    add test sum, arr

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function sum (n1, n2){
     return n1 + n2;
 }
 
+function allUpper(arr){
+    return arr.map(el => el.toUpperCase())
+}
+
 // console.log(sum(1,2));
 
-module.exports = sum;
+module.exports = {sum, allUpper};
 
